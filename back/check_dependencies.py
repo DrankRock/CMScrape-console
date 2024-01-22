f7b88d0cd5e2c96ac67a6a9dfbb068f509db1201
DrankRock <32172257+DrankRock@users.noreply.github.com>
2024-01-22 15:04:40 +0100

    Feat: Update requirements to have the full req + sub-req and update check_dependencies to install libxml2

--- back/check_dependencies.py
+++ back/check_dependencies.py
@@ -27,11 +27,34 @@ def update_libseccomp():
             # Update libseccomp2
             update_command = "sudo apt install libseccomp2=2.5.1-1~bpo10+1"  # You can adjust the version as needed
             subprocess.run(update_command, shell=True, check=True)
-            print(f"libseccomp2 updated to version 2.5.1-1~bpo10+1. Please restart the Docker daemon and the container.")
+            print(f"libseccomp2 updated to version 2.5.1-1~bpo10+1.")
 
     except subprocess.CalledProcessError as e:
         print(f"Error: {e}")
 
+def install_libxml2():
+    try:
+        # Check libxml2 version using apt-cache policy
+        check_command = "apt-cache policy libxml2"
+        result = subprocess.run(check_command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+
+        # Parse the output to get the installed version
+        installed_version_line = next(line for line in result.stdout.split('\n') if 'Installed:' in line)
+        installed_version = installed_version_line.split()[-1]
+
+        # Check if the version is already 2.9.x
+        if not installed_version.startswith("2.9."):
+            # Update libxml2
+            update_command = "sudo apt install libxml2"
+            subprocess.run(update_command, shell=True, check=True)
+            print(f"libxml2 installed")
+
+    except subprocess.CalledProcessError as e:
+        print(f"Error: {e}")
+
+
 if __name__ == "__main__":
-    check_docker_installed()
+    install_libxml2()
     update_libseccomp()
+    check_docker_installed()
+    
